f55b8bbb6676e312195848be4892deaf13eac24b
Jason Lam <nyjasonlam@gmail.com>
2021-05-20 17:55:37 -0400

    Added onDelete for category

--- models/index.js
+++ models/index.js
@@ -5,20 +5,22 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
-Product.belongsTo(Category, { foreignKey: "category_id" });
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
-Category.hasMany(Product, { foreignKey: "category_id" });
+Category.hasMany(Product, { foreignKey: "category_id", onDelete: "SET NULL" });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_id',
+  foreignKey: "product_id",
   through: ProductTag,
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'tag_id',
+  foreignKey: "tag_id",
   through: ProductTag,
 });
 
