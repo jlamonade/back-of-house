9e3b1e74a679134bf7408e23aca50941317c30c3
Jason Lam <nyjasonlam@gmail.com>
2021-05-19 20:48:12 -0400

    added get category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,13 +14,26 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      where: {
+        id: req.params.id,
+      }
+    })
+    res.json(categoryData)
+  } catch {
+    res.status(500).send("500 Internal Server Error.")
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await
+  }
 });
 
 router.put('/:id', (req, res) => {
