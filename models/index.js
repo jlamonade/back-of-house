530a61fe25c3f97d3bd0703c2b278835a18b06ae
Jason Lam <nyjasonlam@gmail.com>
2021-05-20 13:26:16 -0400

    Added foreign key associations in model associations for product and tag

--- models/index.js
+++ models/index.js
@@ -8,13 +8,19 @@ const ProductTag = require("./ProductTag");
 Product.belongsTo(Category, { foreignKey: "category_id" });
 
 // Categories have many Products
-Category.hasMany(Product, { foreignKey: "category_id" });
+Category.hasMany(Product, { foreignKey: "category_id", onDelete: "SET NULL" });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  foreignKey: "tag_id",
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  foreignKey: "product_id",
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
